7ded085c09442ae23d9637d984b71a0745accd3a
nishkalprakash <nishkalprakash@gmail.com>
2023-07-18 12:13:24 +0530

    fixed roi code to use file names instead of path when checking for completed files

--- ROI_extractor_main.py
+++ ROI_extractor_main.py
@@ -1,4 +1,10 @@
 ## CODE CREATER: Nishkal Prakash (nishkal@iitkgp.ac.in)
+## Steps to reproduce
+## run in terminal
+## conda create -n roi python -y
+## conda activate roi
+## pip install -r requirements_roi.txt
+
 
 # from minutiae_extractor import extract_minutiae_vector as emv
 from pathlib import Path
@@ -6,34 +12,45 @@ from pathlib import Path
 from ROI import ROI_extractor
 import cv2
 
+
 if __name__ == "__main__":
     from BI import Parallel
 
     def get_roi(input_folder_path, output_folder_path):
-        all_paths_set = set(input_folder_path.rglob('*.*'))
+        all_img_names_set = set([file.name for file in input_folder_path.rglob('*.*') if file.suffix in ['.tif','.bmp','.png']])
     
-        proc_set = set(output_folder_path.rglob('*.*'))
-        to_process = all_paths_set - proc_set
+        proc_set = set([file.name for file in output_folder_path.rglob('*.*')])
+        to_process = [input_folder_path/path for path in (all_img_names_set - proc_set)]
+        print(f"Total files to process: {len(to_process)} out of {len(all_img_names_set)}")
         ll=Parallel(debug=False)
-        for doc_list in ll(ROI_extractor,to_process, 200):
+        for doc_list in ll(ROI_extractor,to_process, 50):
             # try:
             for cropped_img,path in doc_list:
                 out_path=output_folder_path/path.name
                 # path=Path(path)
                 ## check if shape of cropped image is not 0
-                if cropped_img.shape==0 or cropped_img.shape[0]==0 or cropped_img.shape[1]==0:
+                try:
+                    if cropped_img.shape==0 or cropped_img.shape[0]==0 or cropped_img.shape[1]==0:
+                        print(f"Failed for {path.parent.parent/path.name}")
+                    else:
+                        cv2.imwrite(out_path.as_posix(),cropped_img)
+                        print('.',end='')
+                except:
                     print(f"Failed for {path.parent.parent/path.name}")
-                else:
-                    cv2.imwrite(out_path.as_posix(),cropped_img)
+                # if cropped_img.shape==0 or cropped_img.shape[0]==0 or cropped_img.shape[1]==0:
+                #     print(f"Failed for {path.parent.parent/path.name}")
+                # else:
+                #     cv2.imwrite(out_path.as_posix(),cropped_img)
+                #     print('.',end='')
 
-                
+    base_path = Path("D:/FVC Fingerprint Datasets")
     input_paths=[
-        Path(r"Y:\FVC Fingerprint Datasets\FVC2006\Dbs\DB2_A"),
-        Path(r"Y:\FVC Fingerprint Datasets\FVC2004\Dbs\DB2_A"),
+        base_path/"FVC2006/Dbs/DB2_A",
+        base_path/"FVC2004/Dbs/DB2_A",
     ]
     output_paths=[
-        Path(r"Y:\FVC Fingerprint Datasets\FVC2006\Dbs\DB2_A_ROI"),
-        Path(r"Y:\FVC Fingerprint Datasets\FVC2004\Dbs\DB2_A_ROI")
+        base_path/"FVC2006/Dbs/DB2_A_ROI",
+        base_path/"FVC2004/Dbs/DB2_A_ROI",
     ]
     ## create output paths if not exists
     for p in output_paths:
--- vars.py
+++ vars.py
@@ -6,7 +6,7 @@ DB_IP = "10.5.18.101"
 
 
 ## Change as needed
-DS_PREFIX = Path("~/datasets").expanduser()
+DS_PREFIX = Path("D:/").expanduser()
 
 ## List of Available datasets
 AVAIL_DS = {_.name for _ in DS_PREFIX.iterdir() if _.is_dir()}
